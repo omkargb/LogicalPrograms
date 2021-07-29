82a85b962b4dbd02e210e55d191d98a94cf67fdf
omkargb <omkargb107@gmail.com>
2021-07-29 20:56:54 +0530

    [omkargb] Add : Welcome message displayed and program files uploaded

---
+++ LogicalProgramsDay6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LogicalProgramsDay6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Logical Programming Problems ");
+            Console.WriteLine("\n Here is the list of programs : ");
+            Console.WriteLine( " 1. Fibonacci Series\n" +
+                " 2. Perfect Number\n" +
+                " 3. Prime Number\n" +
+                " 4. Reverse a number\n" +
+                " 5. Coupon Numbers\n" +
+                " 6. Simulate Stopwatch Program \n");
+        }
+    }
+}
